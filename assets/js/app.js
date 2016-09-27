20c3538a7afbf6790110636466262773a9991fdd
Ednas <edna@edna.tech>
2016-09-27 16:51:25 -0400

    Working on game logic, making guesses into array and deducting num of guesses left

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,82 +11,89 @@ var letterToGuess = null;
 
 
 
-//When key is released it becomes the users guess
-document.onkeyup = function(event) {
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-
 //Lets the computer select a random letter from the available choices
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 //Allows the user 9 guesses
-    guesses = guesses || 9
-    var updateGuessesLeft = function(){
-        // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
-        document.querySelector('#guessLeft').innerHTML = guessesLeft;
-    };
+// guesses = guesses || 9
+var updateGuessesLeft = function() {
+  // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
+  document.querySelector('#guessLeft').innerHTML = "Hello " + guessesLeft;
+  // document.getElementById('#guessLeft').append = "Hello " +guessesLeft;
+};
 
-    var updateLetterToGuess = function(){
-        // Here we get a random letterToGuess and assign it based on a random generator (only looking at a, b, or c)
-        this.letterToGuess = this.letters[Math.floor(Math.random() * this.letters.length)];
-    };
-    var updateGuessesSoFar = function(){
-        // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
-        document.querySelector('#let').innerHTML = guessedLetters.join(', ');
-    };
-    // Function will be called when we reset everything
-    var reset = function(){
-        totalGuesses = 9;
-        guessesLeft = 9;
-        guessedLetters = [];
-        updateLetterToGuess();
-        updateGuessesLeft();
-        updateGuessesSoFar();
-    }
-    
-    updateLetterToGuess(); 
-    updateGuessesLeft();
+var updateLetterToGuess = function() {
+  this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+};
+var updateGuessesSoFar = function() {
+  // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
+  document.querySelector('#let').innerHTML = guessedLetters.join(', ');
+};
+// Function will be called when we reset everything
+var reset = function() {
+  totalGuesses = 9;
+  guessesLeft = 9;
+  guessedLetters = [];
+  updateLetterToGuess();
+  updateGuessesLeft();
+  updateGuessesSoFar();
+}
 
-    // Repeat the following logic whenever the user guesses incorrectly.
-    while (userGuess !== computerGuess.toString())
-    {
-        --guesses;
-        if (guesses === 0)
-        {
-            {break}
-            alert("The Letter was: " + computerGuess);
-            
-        }
-
-      //COME BACK TO THIS
-      // It's supposed to tally the guesses and make the number go down. 
-      //  userGuess = guesses -;
-    }
+updateLetterToGuess();
+updateGuessesLeft();
 
 
-            if (userGuess == computerGuess){
-            wins++;
-        }else if (userGuess !== computerGuess){
-            losses++;
-        }
-        
+//When key is released it becomes the users guess
+document.onkeyup = function(event) {
+    guessesLeft--;
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+  guessedLetters.push(userGuess);
+  updateGuessesLeft();
+  updateGuessesSoFar();
+  // Repeat the following logic whenever the user guesses incorrectly.
 
 
-        // Taking the tallies and displaying them in HTML
-        var html = "" +
-        "<p>Wins: " + 
-        wins + 
-        "</p>" +
-        "<p>Losses: " + 
-        losses + 
-        "</p>" +
-        "<p>Guesses Left: " + (9 - guesses) 
-        +
-        "<p>Your Guesses so far: " + userGuess
-        guesses +
-        "</p>";
+  // function round() {
+  //   while (userGuess !== computerGuess.toString()) {
+  //     guesses--;
+  //     if (guesses == 0) {
+  //       {
+  //         break }
+  //       alert("The Letter was: " + computerGuess);
 
-        // Placing the html into the game ID
-        document.querySelector('#game').innerHTML = html;
-        
-}    
+  //     }
 
+  //     //COME BACK TO THIS
+  //     // It's supposed to tally the guesses and make the number go down. 
+  //     //  userGuess = guesses -;
+  //   }
+  // }
+  // round();
+
+  // if (userGuess == computerGuess) {
+  //   wins++;
+  // } else if (userGuess !== computerGuess) {
+  //   losses++;
+  // }
+
+
+
+  // Taking the tallies and displaying them in HTML
+  //  var html = "" +
+  // "<p>Wins: " + 
+  // wins + 
+  // "</p>" +
+  // "<p>Losses: " + 
+  // losses + 
+  // "</p>" +
+  // "<p>Guesses Left: " + (9 - guesses) 
+  // +
+  // "<p>Your Guesses so far: " + userGuess
+  // guesses +
+  // "</p>";
+
+  // Placing the html into the game ID
+  //  document.querySelector('#game').innerHTML = html;
+
+}
