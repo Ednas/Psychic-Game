bfab6d768b1ec3d58425147c09cb4ca8e405a6fb
Ednas <edna@edna.tech>
2016-09-27 03:58:07 -0400

    removed hangman, added more variables to psychic game

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>The Psychic Game</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-</head>
-<body>
-<header><h1>The Psychic Game</h1></header>
-
-<!--Empty Div for the tallies-->
-<div id="game">
-	<p id="wins">Wins: </p>
-    <p id="losses">Losses: </p>
-    <p id="guessLeft">Guesses Left: </p>
-    <p id="let">Your Guesses so far: </p>
-</div>
-
-<script type="text/javascript">
-
 //Letter choices available
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 //Setting all to zero
 var wins = 0;
 var losses = 0;
-var guesses = 0;
+var guesses = 9;
+var guessesLeft = 9;
+var guessedLetters = [];
+var letterToGuess = null;
+
+
 
 //When key is released it becomes the users guess
 document.onkeyup = function(event) {
@@ -54,8 +39,6 @@ document.onkeyup = function(event) {
       //  userGuess = guesses -;
     }
 
-    console.log("You must be Psychic");
-
 
             if (userGuess == computerGuess){
             wins++;
@@ -84,8 +67,3 @@ document.onkeyup = function(event) {
         
 }    
 
-</script>
-
-
-</body>
-</html>
--- index.php
+++ index.php
@@ -1,1 +1,1 @@
-<?php include_once("thepsychicgame.html"); ?>
+<?php include_once("index.html"); ?>
